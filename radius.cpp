c85bb26a10e5c7448a89f9f07e658dbe941f6c28
Atia Akter <atiaakter6666@gmail.com>
2026-03-29 11:54:38 +0600

    radius

---
+++ radius.cpp
@@ -0,0 +1,11 @@
+#include<iostream>
+#include<cmath>
+using namespace std;
+int main(){
+double C,r,pi=3.1416;
+cout<<"Enter radius of circle:";
+cin>>r;
+C=2*pi*r;
+cout<<"Circumference of circle = "<<C;
+return 0;
+}
